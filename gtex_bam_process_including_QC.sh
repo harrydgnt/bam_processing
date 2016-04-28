28c7aa6d1581c16fa5a45df61f084f08d9c77761
Harry Yang <harry2416@gmail.com>
2016-04-28 00:02:59 -0700

    Clean up
    Some clean up

--- gtex_bam_process_including_QC.sh
+++ gtex_bam_process_including_QC.sh
@@ -27,12 +27,12 @@ itemname=$(echo $item | awk -F '.' '{print $1}')
 ext_item_name=$(echo $item | awk -F '.bam' '{print $1}')
 echo $item 
 
-###each sample would have dedicated folder
+### each sample has dedicated folder
 
 mkdir $ext_item_name
 cd $ext_item_name
 
-###decrypt .ncbi_enc file to bam
+### decrypt .ncbi_enc file to bam
 echo "mv ${1}/${line} ${decdir}" >> run_${itemname}.sh
 echo "cd ${decdir}" >> run_${itemname}.sh
 echo "${decdir}/vdb-decrypt -f ${decdir}/${line}" >> run_${itemname}.sh 
@@ -40,48 +40,49 @@ echo "mv ${decdir}/$item ${dumpdir}/$ext_item_name" >> run_${itemname}.sh
 
 
 
-###HTSeq -default gtf is set
+### HTSeq - default gtf is set
 
-gtf=/u/home/s/serghei/project/Homo_sapiens/Ensembl/GRCh37/Annotation/Genes/genes.gtf 
+gtf = /u/home/s/serghei/project/Homo_sapiens/Ensembl/GRCh37/Annotation/Genes/genes.gtf 
 
-echo "#!/bin/bash">>run_${itemname}.sh
-echo ". /u/local/Modules/default/init/modules.sh" >>run_${itemname}.sh
-echo "module load samtools" >>run_${itemname}.sh
-echo "module load bamtools" >>run_${itemname}.sh
+echo "#!/bin/bash" >> run_${itemname}.sh
+echo ". /u/local/Modules/default/init/modules.sh" >> run_${itemname}.sh
+echo "module load samtools" >> run_${itemname}.sh
+echo "module load bamtools" >> run_${itemname}.sh
 echo "cd ${dumpdir}/${ext_item_name}" >> run_${itemname}.sh 
-echo "samtools sort -n ${dumpdir}/${ext_item_name}/$item ${dumpdir}/${ext_item_name}/${item}_sort_byname">>run_${itemname}.sh
-echo "module load python/2.7.3">>run_${itemname}.sh
-echo "python /u/home/h/harryyan/project-eeskin/utilities/HTSeq-0.6.1/scripts/htseq-count --format=bam --mode=intersection-strict --stranded=no ${dumpdir}/${ext_item_name}/${item}_sort_byname.bam $gtf >${dumpdir}/${ext_item_name}/${item}.counts" >>run_${itemname}.sh
-echo "rm -rf ${dumpdir}/${ext_item_name}/${item}_sort_byname.bam">>run_${itemname}.sh
+echo "samtools sort -n ${dumpdir}/${ext_item_name}/$item ${dumpdir}/${ext_item_name}/${item}_sort_byname" >> run_${itemname}.sh
+echo "module load python/2.7.3" >> run_${itemname}.sh
+echo "python /u/home/h/harryyan/project-eeskin/utilities/HTSeq-0.6.1/scripts/htseq-count --format=bam --mode=intersection-strict --stranded=no ${dumpdir}/${ext_item_name}/${item}_sort_byname.bam $gtf >${dumpdir}/${ext_item_name}/${item}.counts" >> run_${itemname}.sh
+echo "rm -rf ${dumpdir}/${ext_item_name}/${item}_sort_byname.bam" >> run_${itemname}.sh
+
+
 
 
 ### bam index
 
-echo "samtools index ${dumpdir}/${ext_item_name}/${item}">>run_${itemname}.sh
+echo "samtools index ${dumpdir}/${ext_item_name}/${item}" >> run_${itemname}.sh
+
+
+
 
 ### unmapped read
 
-echo "num_unmapped=\$(samtools view -c -fox4 ${dumpdir}/${ext_item_name}/${item})">>run_${itemname}.sh
-echo "num_total=\$(samtools view -c ${dumpdir}/${ext_item_name}/${item})">>run_${itemname}.sh
-echo "echo "\${num_unmapped}	\${num_total}">>${dumpdir}/unmapped_ratio.txt">>run_${itemname}.sh
-#
-###gene profile 
-#echo "mkdir ${dumpdir}/${ext_item_name}/genomic_profile">>run_${itemname}.sh
-#echo "python /u/home/h/harryyan/project-eeskin/repeat/gprofile/gprofilePE.py --readPerCategory ${dumpdir}/${ext_item_name}/${item} ${dumpdir}/${ext_item_name}/genomic_profile/${ext_item_name}_genome h">>run_${itemname}.sh
+echo "num_unmapped=\$(samtools view -c -fox4 ${dumpdir}/${ext_item_name}/${item})" >> run_${itemname}.sh
+echo "num_total=\$(samtools view -c ${dumpdir}/${ext_item_name}/${item})" >> run_${itemname}.sh
+echo "echo "\${num_unmapped}	\${num_total}" >> ${dumpdir}/unmapped_ratio.txt" >> run_${itemname}.sh
 
 
 ### repeat profile
-echo "mkdir ${dumpdir}/${ext_item_name}/repeat_profile">>run_${itemname}.sh
-echo "python /u/home/h/harryyan/project-eeskin/repeat/rprofile/rprofile.py ${dumpdir}/${ext_item_name}/${item} ${dumpdir}/${ext_item_name}/repeat_profile/${ext_item_name}_repeat.">>run_${itemname}.sh
+
+echo "mkdir ${dumpdir}/${ext_item_name}/repeat_profile" >> run_${itemname}.sh
+echo "python /u/home/h/harryyan/project-eeskin/repeat/rprofile/rprofile.py ${dumpdir}/${ext_item_name}/${item} ${dumpdir}/${ext_item_name}/repeat_profile/${ext_item_name}_repeat." >> run_${itemname}.sh
 
 
 
 
 
-###bam file extractor for each gene
+### bam file extractor for each gene
 while read gene
 do
-###becho $gene
 chr=$(echo $gene | awk -F ',' '{print $1}')
 name=$(echo $gene | awk -F ',' '{print $4}')
 pos_one=$(echo $gene | awk -F ',' '{print $5}')
@@ -95,7 +96,7 @@ done<$5
 
 ### extract unmapped bam - unmappped fastq extraction has been disabled
 echo "samtools view -b -f 4 ${dumpdir}/${ext_item_name}/${item} > ${dumpdir}/${ext_item_name}/${ext_item_name}.unmapped.bam" >> run_${itemname}.sh
-echo "samtools index ${dumpdir}/${ext_item_name}/${ext_item_name}.unmapped.bam">>run_${itemname}.sh
+echo "samtools index ${dumpdir}/${ext_item_name}/${ext_item_name}.unmapped.bam" >> run_${itemname}.sh
 #echo "bamtools convert -in ${dumpdir}/${ext_item_name}/${ext_item_name}.unmapped.bam -format fastq > ${dumpdir}/${ext_item_name}/${ext_item_name}.fastq">>run_${itemname}.sh
 #echo "gzip ${dumpdir}/${ext_item_name}/${ext_item_name}.fastq">>run_${itemname}.sh
 #echo "rm ${dumpdir}/${ext_item_name}/${ext_item_name}.bam">>run_${itemname}.sh
@@ -121,10 +122,10 @@ echo "samtools index ${dumpdir}/${ext_item_name}/${ext_item_name}.unmapped.bam">
 
 echo "/u/home/h/harryyan/project-eeskin/rop/rop.py --b --qsubArray ${dumpdir}/${ext_item_name}/${ext_item_name}.unmapped.bam ${dumpdir}/${ext_item_name}/rop/" >> run_${itemname}.sh
 
+### TODO - remove intermediate files
 
 
-
-###gene profile
+### genomic profile
 echo "mkdir ${dumpdir}/${ext_item_name}/genomic_profile">>run_${itemname}.sh
 echo "python /u/home/h/harryyan/project-eeskin/repeat/gprofile/gprofilePE.py --readPerCategory ${dumpdir}/${ext_item_name}/${item} ${dumpdir}/${ext_item_name}/genomic_profile/${ext_item_name}_genome h">>run_${itemname}.sh
 echo "rm ${dumpdir}/${ext_item_name}/${ext_item_name}.bam">>run_${itemname}.sh
